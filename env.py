560d007cb632cd44dbbdfd6900d6bb83867bc3c1
Tafari-K <kirton.tafari@gmail.com>
2026-01-01 22:11:23 +0000

    created env.py, .gitignore files, connected database to code. DEBUG > False

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_VXReN7kFo2Jd@ep-curly-union-ag0w43zp.c-2.eu-central-1.aws.neon.tech/panda_scarf_seven_768747"
+)
