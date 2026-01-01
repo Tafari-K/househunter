9e48ed4b44b75679cfabebaf4272f5860de2d28a
Tafari-K <kirton.tafari@gmail.com>
2026-01-01 23:34:12 +0000

    Added comments model, updated admin.py, migrated. server working

--- blog/models.py
+++ blog/models.py
@@ -6,16 +6,32 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
+    excerpt = models.TextField(blank=True)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='posts')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=((0, 'Draft'), (1, 'Published')), default=0)
     updated_on = models.DateTimeField(auto_now=True)
-    excerpt = models.TextField(blank=True)
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
-        return f"The title of this post is {self.title} | posted by {self.author.username}"
+        return f"The title of this post is {self.title} | posted by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="commenter")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.author}"
