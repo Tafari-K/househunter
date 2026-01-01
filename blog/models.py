e5acaa26e2a05e6541fa7b4fc723930934ca4ca1
Tafari-K <kirton.tafari@gmail.com>
2026-01-01 22:53:59 +0000

    Added post model, registered in admin.py, migrated, server working, DEBUG > False

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=((0, 'Draft'), (1, 'Published')), default=0)
+
+    def __str__(self):
+        return self.title
