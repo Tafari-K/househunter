aeb4d8d516c9826bf00b865bec5e93770ea33e65
Tafari-K <kirton.tafari@gmail.com>
2026-01-03 16:03:13 +0000

    Add contents to README.md. DEBUG > True

--- env.py
+++ env.py
@@ -8,5 +8,5 @@ os.environ.setdefault(
     "SECRET_KEY", "3vn)es-4no33xim_j#w-n_z6kvqcxr8bcfq6m-rf29_)@3^e%p"
 )
 os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://337167813735684:jE3ra4X8LO2yr-KfAoZqW2tHjak"
+    "CLOUDINARY_URL", "cloudinary://337167813735684:jE3ra4X8LO2yr-KfAoZqW2tHjak@drk5a3mpf"
 )
