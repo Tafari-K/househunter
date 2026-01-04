65bc4ce738e0896ec1bf281fc3e2ac7661a235ef
Tafari-K <kirton.tafari@gmail.com>
2026-01-04 20:46:19 +0000

    removed redundanct functions. DEBUG > False

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,6 @@ from .models import Post, Comment
 from .forms import CommentForm
 
 
-
 # Create your views here.
 
 
@@ -70,7 +69,7 @@ def comment_edit(request, slug, comment_id):
             comment.save()
             messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
         else:
-            messages.add_message(request, messages.ERROR, 
+            messages.add_message(request, messages.ERROR,
                                  'Error updating comment!')
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
@@ -80,16 +79,14 @@ def comment_delete(request, slug, comment_id):
     """
     view to delete comment
     """
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
 
     if comment.author == request.user:
         comment.delete()
-        messages.add_message(request, messages.SUCCESS, 
+        messages.add_message(request, messages.SUCCESS,
                              'Comment deleted! This cannot be undone.')
     else:
-        messages.add_message(request, messages.ERROR, 
+        messages.add_message(request, messages.ERROR,
                              'You can only delete your own comments!')
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
