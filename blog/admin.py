4d205e1c5ed4dcdcfd854dd0a7fdf6d124c2da9d
Tafari-K <kirton.tafari@gmail.com>
2026-01-05 05:53:12 +0000

    Fix admin post links

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'author', 'status', 'created_on')
+    list_display_links = ['title']
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on', 'author')
     prepopulated_fields = {'slug': ('title',)}
