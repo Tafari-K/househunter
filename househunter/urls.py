b9beaf8c5c151cc0791b54701f5123e6409aa394
Tafari-K <kirton.tafari@gmail.com>
2026-01-05 03:52:47 +0000

    updated urls for cloudinary

--- househunter/urls.py
+++ househunter/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('about/', about_views.about, name='about'),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('cloudinary/', include('cloudinary_storage.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', include("blog.urls"), name="blogs-urls"),
 ]
