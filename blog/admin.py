808fad0b6761198794eda18cc856d7935a8ef79c
Tafari-K <kirton.tafari@gmail.com>
2026-01-05 06:08:12 +0000

    Fix admin summernote permission error

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
-# Register your models here.
 
 
 @admin.register(Post)
@@ -12,6 +11,12 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on', 'author')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+    
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        # Fix for Summernote permission issue
+        if request is None:
+            return super().formfield_for_dbfield(db_field, None, **kwargs)
+        return super().formfield_for_dbfield(db_field, request, **kwargs)
 
 
 admin.site.register(Comment)
