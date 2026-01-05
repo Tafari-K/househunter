4ad1ed85ecee422d7695c626f465b1ebb2bc3b1b
Tafari-K <kirton.tafari@gmail.com>
2026-01-05 05:30:47 +0000

    Temporarily simplify Post admin to fix 500 error

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,5 @@
 from django.contrib import admin
 from .models import Post, Comment
-from django_summernote.admin import SummernoteModelAdmin
-# Register your models here.
 
-
-@admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'author', 'status', 'created_on')
-    search_fields = ['title', 'content']
-    list_filter = ('status', 'created_on', 'author')
-    prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
-
-
+admin.site.register(Post)
 admin.site.register(Comment)
